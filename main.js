5d95e84b00dd0a3b5ae74f2d44145bdd34736444
TolikBu <tolikelement@gmail.com>
2021-12-03 00:50:31 +0500

    feat main.js fix

--- main.js
+++ main.js
@@ -1,27 +1,24 @@
 "use strict";
-let setNum;
 
-const play = function () {
-  let number = 50;
-  return function () {
-    setNum = prompt("Угадай число от 1 до 100");
-    console.log(setNum);
+function play(number) {
+  return (function option() {
+    const setNum = prompt("Угадай число от 1 до 100");
     if (isNaN(setNum)) {
       alert("Введите число");
-      play();
+      option();
     } else if (setNum == null) {
       alert("Игра окончена");
     } else if (setNum > number) {
       alert("Загаданное число меньше");
-      play();
+      option();
     } else if (setNum < number) {
       alert("Загаданное число больше");
-      play();
+      option();
     } else if (setNum == number) {
       alert("Поздравляю, Вы угадали!!!");
     }
-  } ();
-};
+    console.log(setNum);
+  })();
+}
 
-const game = play();
-
+const game = play(50);

d416a383be7d51b5384ca64869feb9d9ddbc10b5
TolikBu <tolikelement@gmail.com>
2021-12-02 23:34:39 +0500

    feat main.js: game play

--- main.js
+++ main.js
@@ -1,2 +1,26 @@
-let setNum = prompt("Угадай число от 1 до 100");
+"use strict";
+let setNum;
 
+const play = function () {
+  let number = 50;
+  return function () {
+    setNum = prompt("Угадай число от 1 до 100");
+    console.log(setNum);
+    if (isNaN(setNum)) {
+      alert("Введите число");
+      play();
+    } else if (setNum == null) {
+      alert("Игра окончена");
+    } else if (setNum > number) {
+      alert("Загаданное число меньше");
+      play();
+    } else if (setNum < number) {
+      alert("Загаданное число больше");
+      play();
+    } else if (setNum == number) {
+      alert("Поздравляю, Вы угадали!!!");
+    }
+  } ();
+};
+
+play();

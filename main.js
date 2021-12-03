0effdbe895e84a6c11babbde66d568d871190fe8
TolikBu <tolikelement@gmail.com>
2021-12-03 17:18:49 +0500

    fix main.js: fix gemeplay

--- main.js
+++ main.js
@@ -6,20 +6,19 @@ function play() {
     if (isNaN(userNum)) {
       alert("Введите число");
       return check(item);
-      // return;
+    } else if (userNum === null) {
+      alert("Игра окончена");
+    } else if (userNum == item) {
+      confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
+    }  else if (userNum == "") {
+      prompt("Надо ввесит число от 1 до 100");
+      return check(item);
     } else if (userNum > item) {
       alert("Загаданное число меньше");
       return check(item);
-      // return;
     } else if (userNum < item) {
       alert("Загаданное число больше");
       return check(item);
-      // return;
-    } else if (userNum == item) {
-      confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-    } else if (userNum === null) {
-      alert("Игра окончена");
-      console.log(userNum);
     }
   };
 }

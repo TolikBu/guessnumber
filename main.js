9a03b44396e7c5fddc81d76c1c2efa9a6d3e54a7
TolikBu <tolikelement@gmail.com>
2021-12-05 23:35:45 +0500

    feat main.js: complicated guessnumber

--- main.js
+++ main.js
@@ -1,27 +1,44 @@
 "use strict";
 
 function play() {
+  let lives = 10;
+    function attempt() {
+      lives--;
+      if (lives <= 0) {
+        confirm("Попытка закончились хотете сыгграть еще раз?");
+        lives = 10;
+        return play();
+      } 
+  }
+
   return function check(item) {
     const userNum = prompt("Угадайте число от 1 до 100");
     if (isNaN(userNum)) {
       alert("Введите число");
       return check(item);
-    } else if (userNum === null) {
-      alert("Игра окончена");
-    } else if (userNum == item) {
+    }  else if (userNum == item) {
       confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-    }  else if (userNum == "") {
-      prompt("Надо ввесит число от 1 до 100");
+      lives = 10; 
+      return play();
+    } else if (userNum === null) {
+      alert("Всего хорошо");
+    } else if (userNum == "") {
+      alert("Надо ввесит число от 1 до 100");
       return check(item);
     } else if (userNum > item) {
-      alert("Загаданное число меньше");
+      alert("Загаданное число меньше, осталось попыток" + " " + (lives - 1));
+      attempt();
       return check(item);
     } else if (userNum < item) {
-      alert("Загаданное число больше");
+      alert("Загаданное число больше, осталось попыток" + " " + (lives - 1));
+      attempt();
       return check(item);
     }
   };
+
 }
+
 
 const game = play();
 game(50);
+

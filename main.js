e0eae4b0acde03351e829afe7906e5a85c079a06
TolikBu <tolikelement@gmail.com>
2021-12-07 01:20:55 +0500

    fix main.js: fix guessnumber

--- main.js
+++ main.js
@@ -2,24 +2,34 @@
 
 function play() {
   let lives = 10;
-    function attempt() {
-      lives--;
-      if (lives <= 0) {
-        confirm("Попытка закончились хотете сыгграть еще раз?");
-        lives = 10;
-        return play();
-      } 
-  }
-
+  
   return function check(item) {
+    function attempt() { 
+      lives--;
+      console.log("lives", lives);
+      if (lives <= 0) {
+        if (confirm("Попытка закончились хотете сыгграть еще раз?")) {
+          lives = 10;
+          play(); 
+        } else {
+          return;
+        }
+      } 
+    }
+
+    if (lives <= 0) return;
+
     const userNum = prompt("Угадайте число от 1 до 100");
     if (isNaN(userNum)) {
       alert("Введите число");
       return check(item);
-    }  else if (userNum == item) {
-      confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?");
-      lives = 10; 
-      return play();
+    } else if (userNum == item) {
+      if (confirm("Поздравляю, Вы угадали!!! Хотели бы сыграть еще?")) {
+        lives = 10;
+        check(item);
+      } else {
+        return;
+      }
     } else if (userNum === null) {
       alert("Всего хорошо");
     } else if (userNum == "") {
@@ -35,7 +45,6 @@ function play() {
       return check(item);
     }
   };
-
 }
 
 
